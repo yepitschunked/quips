515ff81f5e5d1a0b29dd4e31e9194b711357bb27
Victor Lin <victor@coupa.com>
2011-10-08 15:55:41 -0700

    prepare_hovers is retarded, replace with css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function prepare_hovers() {
-    var old_plus = $('addnew_plus').getStyle('color');
-    var old_O = $('random_O').getStyle('color');
-    $('addnew').observe('mouseover', function() {
-        $('addnew_plus').setStyle({
-            color: '#007711',
-        });
-    });
-    $('addnew').observe('mouseout', function() {
-        $('addnew_plus').setStyle({
-            color: old_plus,
-        });
-    });
-    $('random').observe('mouseover', function() {
-        $('random_O').setStyle({
-            color: '#001177',
-        });
-    });
-    $('random').observe('mouseout', function() {
-        $('random_O').setStyle({
-            color: old_O,
-        });
-    });
-}
