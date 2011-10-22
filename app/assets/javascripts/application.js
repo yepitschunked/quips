1c960e139c8bac457007a92b3ccd17fea71603e9
Victor Lin <victor@coupa.com>
2011-10-21 20:48:02 -0700

    fuck it, upgrade to rails 3.1

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,17 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+//voting
+$(function() {
+  function disable_voting() {
+    $('#upvote').html('');
+    $('#downvote').html('');
+  }
+
+  $('#upvote').bind('ajax:loading', function() { disable_voting(); });
+  $('#upvote').bind('ajax:complete', function(data) { $('#votes').html(data); });
+  $('#search_box').focus(function(e) {
+      $(this).css({color: "black"});
+      $(this).value="";
+  });
+});
