177631316165178e2575620a77ae9778425873e4
Victor Lin <victorhlin@gmail.com>
2013-02-10 13:00:07 -0800

    use ssh repo url instead of https

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,15 @@
 set :application, "quips"
 set :user, "deploy"
 set :use_sudo, false
-set :repository,  "https://wonnage@github.com/wonnage/quips.git"
+set :repository,  "git@github.com:wonnage/quips.git"
 set :deploy_to, "/var/rails/#{application}"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "victor-lin.cust.arpnetworks.com"                          # Your HTTP server, Apache/etc
-role :app, "victor-lin.cust.arpnetworks.com"                          # This may be the same as your `Web` server
-role :db,  "victor-lin.cust.arpnetworks.com", :primary => true # This is where Rails migrations will run
-role :db,  "victor-lin.cust.arpnetworks.com"
+role :web, "resquip.stillraging.net"                          # Your HTTP server, Apache/etc
+role :app, "resquip.stillraging.net"                          # This may be the same as your `Web` server
+role :db,  "resquip.stillraging.net", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
