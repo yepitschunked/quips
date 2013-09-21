69c7bf7653a5042b1a95d3bb25e109724478632a
Victor Lin <victorhlin@gmail.com>
2013-09-21 16:01:20 -0700

    upgrade rails, angular, fix asset compilation issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require angular
+//= require quips_index
 
 $(function() { $('.datepicker').datepicker(); });
 /* this allows us to pass in HTML tags to autocomplete. Without this they get escaped */
